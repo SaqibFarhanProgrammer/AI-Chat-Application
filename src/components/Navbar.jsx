16b626ea0be5cc4042d19951208cf34d5b4dbc80
shahzaib khan <shahzaibshazzo@gmail.com>
2025-11-19 17:33:09 +0500

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from "..//assets/cions/logo.jpg";
 function Navbar() {
   return (
     <div
@@ -8,9 +8,9 @@ function Navbar() {
     
     flex items-center  mx-auto px-4 py-3 border-b border-white/10 max-w-[100vw] justify-end"
     >
-      <h1 className="text-lg sm:text-xl max-[420px]:pt-0.5 py-2 font-semibold tracking-wide">
-        Neura AI
-      </h1>
+      <div className="text-lg sm:text-xl max-[420px]:pt-0.5 py-2 font-semibold tracking-wide">
+        <img className="w-[100%] h-10 max-[420px]:h-6" src={logo} alt="" />
+      </div>
     </div>
   );
 }

02b77f3177b206b36dbe438cfd1493901993d469
shahzaib khan <shahzaibshazzo@gmail.com>
2025-11-19 14:44:27 +0500

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div className="w-[100%] 
     
     flex items-center  mx-auto px-4 py-3 border-b border-white/10 max-w-[100vw] justify-end">
-      <h1 className="text-lg sm:text-xl pt-0.5 font-semibold tracking-wide">
+      <h1 className="text-lg sm:text-xl max-[420px]:pt-0.5 py-2 font-semibold tracking-wide">
         Neura AI
       </h1>
     </div>

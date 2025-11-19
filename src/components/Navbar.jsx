2a9141672328313e891d49463cdb4c165178eeb9
shahzaib khan <shahzaibshazzo@gmail.com>
2025-11-19 20:56:02 +0500

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       className="w-[100%] 
       navbar
     
-    flex items-center  mx-auto px-4 py-3 border-b border-white/10 max-w-[100vw] justify-end"
+    flex items-center  mx-auto px-4 py-3  max-w-[100vw] justify-end"
     >
       <div className="text-lg sm:text-xl max-[420px]:pt-0.5 py-2 font-semibold tracking-wide">
         <img className="w-[100%] h-10 max-[420px]:h-6" src={logo} alt="" />
